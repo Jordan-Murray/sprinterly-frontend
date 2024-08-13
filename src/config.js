282cc742efeaad6645d061529e9951bb26554143
Jordan Murray <Jordan.Murray@streets-heaver.com>
2024-08-13 15:33:54 +0100

    Return api url to hosted

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
-    API_BASE_URL: 'https://localhost:7033/api/Streets-Heaver/31b8a614-ef73-47ad-8375-9fe47cb4a2d5',
-    // API_BASE_URL: 'https://app-sprinterlyapi-dev-uksouth.azurewebsites.net/api/Streets-Heaver/31b8a614-ef73-47ad-8375-9fe47cb4a2d5',
+    // API_BASE_URL: 'https://localhost:7033/api/Streets-Heaver/31b8a614-ef73-47ad-8375-9fe47cb4a2d5',
+    API_BASE_URL: 'https://app-sprinterlyapi-dev-uksouth.azurewebsites.net/api/Streets-Heaver/31b8a614-ef73-47ad-8375-9fe47cb4a2d5',
   };
   
   export default config;
